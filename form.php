de4f9b74ca9bd35e281f8d870cc834292033d77b
Emi <emohamadz2012@gmail.com>
2022-01-07 22:19:00 +0330

    filter added....

--- form.php
+++ form.php
@@ -16,47 +16,7 @@
         <header>
 
 
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">فرم استخدامی</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Link</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                                    data-bs-toggle="dropdown" aria-expanded="false">
-                                    Dropdown
-                                </a>
-                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a class="dropdown-item" href="#">Action</a></li>
-                                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                                    <li>
-                                        <hr class="dropdown-divider">
-                                    </li>
-                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                                </ul>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link disabled">Disabled</a>
-                            </li>
-                        </ul>
-                        <form class="d-flex">
-                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                            <button class="btn btn-outline-success" type="submit">Search</button>
-                        </form>
-                    </div>
-                </div>
-            </nav>
+       <?php include "header.php"; ?>
 
 
         </header>
@@ -66,7 +26,7 @@
             <div class="row justify-content-center">
                 <div class="col-md-6">
 
-                    <form class="row g-3">
+                    <form class="row g-3" method="POST" action="show.php">
                         <div class="col-md-6">
                             <label for="inputEmail4" class="form-label">نام و نام خانوادگی</label>
                             <input type="text" class="form-control" id="inputEmail4" name="fullname">
@@ -89,8 +49,8 @@
                         <div class="col-6">
                             <label for="inputState" class="form-label">جنسیت</label>
                             <select id="inputState" class="form-select" name="sex">
-                                <option value="male">مرد</option>
-                                <option value="female">زن</option>
+                                <option value="male" >مرد</option>
+                                <option value="female"  >زن</option>
                             </select>
                         </div>
 
---
+++ show.php
@@ -0,0 +1,154 @@
+<?php
+
+include("connect.php");
+
+if (!empty($_POST)) {
+
+
+
+    $fullname = $_POST["fullname"];
+    $age = $_POST["age"];
+    $city = $_POST["city"];
+    $address = $_POST["address"];
+    $sex = $_POST["sex"];
+    $education = $_POST["education"];
+
+
+
+    $sql = "INSERT INTO user (fullname, age, city, address, sex ,education)
+VALUES ('$fullname', '$age', '$city','$address','$sex','$education')";
+
+    $conn->query($sql);
+}
+
+
+
+// if ($conn->query($sql) === TRUE) {
+//   echo "New record created successfully";
+// } else {
+//   echo "Error: " . $sql . "<br>" . $conn->error;
+// }
+
+// $conn->close();
+
+
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <script>
+        $(document).ready(function() {
+            $("#filter").on("keyup", function() {
+                var value = $(this).val().toLowerCase();
+                $("#Table tr").filter(function() {
+                    $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+                });
+            });
+        });
+    </script>
+
+    <title>فرم استخدامی</title>
+</head>
+
+<body dir="rtl">
+
+    <header>
+
+    <?php include "header.php"; ?>
+
+    </header>
+
+
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+
+                <h4 style="text-align: center;" class="text-primary">لیست فرم استخدامی </h4>
+
+                <form class="d-flex mb-5">
+                    <input class="form-control me-2" type="search" aria-label="Search" id="filter">
+                    <button class="btn btn-outline-success" type="submit">فیلتر</button>
+                </form>
+
+
+
+                <table class="table table-success table-striped table-bordered">
+                    <thead>
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">نام و نام خانوادگی </th>
+                            <th scope="col">سن</th>
+                            <th scope="col">شهر</th>
+                            <th scope="col">آدرس</th>
+                            <th scope="col">جنسیت</th>
+                            <th scope="col">مدرک تحصیلی</th>
+                        </tr>
+                    </thead>
+                    <tbody id="Table">
+                        <?php
+
+                        $sql = "SELECT * FROM user";
+                        $result = $conn->query($sql);
+
+                        while ($row = $result->fetch_assoc()) {
+
+                        ?>
+                            <tr>
+                                <th scope="row"><?php echo $row['id']; ?></th>
+                                <td><?php echo $row['fullname']; ?></td>
+                                <td><?php echo $row['age']; ?></td>
+                                <td><?php echo $row['city']; ?></td>
+                                <td><?php echo $row['address']; ?></td>
+                                <td><?php echo $row['sex']; ?></td>
+                                <td><?php echo $row['education']; ?></td>
+                            </tr>
+                        <?php }
+                        ?>
+                    </tbody>
+                </table>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            </div>
+
+
+
+        </div>
+    </div>
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+
+
+</body>
+
+</html>
