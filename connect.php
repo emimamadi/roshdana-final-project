46079924768d63c688bb9a9a0f2563cad8a4508a
Emi <emohamadz2012@gmail.com>
2022-01-08 22:57:25 +0330

    utf8 is added...

--- connect.php
+++ connect.php
@@ -7,6 +7,8 @@ $dbname = "roshdana_final";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
+
+// mysqli_set_charset($conn,"utf8");
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
