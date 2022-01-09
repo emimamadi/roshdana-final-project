17aa53da5d10ead70d972d33dc4986091da3f316
Emi <emohamadz2012@gmail.com>
2022-01-09 22:47:37 +0330

    utf8

--- connect.php
+++ connect.php
@@ -8,7 +8,7 @@ $dbname = "roshdana_final";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// mysqli_set_charset($conn,"utf8");
+mysqli_set_charset($conn,"utf8");
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
