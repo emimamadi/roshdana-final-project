4086e1cd4450a947e1495b9721dcd0570e90418b
Emi <emohamadz2012@gmail.com>
2022-01-09 21:31:36 +0330

    filter edited...

--- index.php
+++ index.php
@@ -1,93 +1,112 @@
+<?php
+
+include "connect.php";
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-            integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-        <title>فرم استخدامی</title>
-    </head>
+    <title>فرم استخدامی</title>
+</head>
 
-    <body dir="rtl">
+<body dir="rtl">
 
-        <header>
-
-
-       <?php include "header.php"; ?>
+    <header>
 
 
-        </header>
+        <?php include "header.php"; ?>
 
 
-        <div class="container mt-5">
-            <div class="row justify-content-center">
-                <div class="col-md-6">
-
-                    <form class="row g-3" method="POST" action="show.php">
-                        <div class="col-md-6">
-                            <label for="inputEmail4" class="form-label">نام و نام خانوادگی</label>
-                            <input type="text" class="form-control" id="inputEmail4" name="fullname">
-                        </div>
-                        <div class="col-md-6">
-                            <label for="inputPassword4" class="form-label">سن</label>
-                            <input type="number" class="form-control" id="inputPassword4" name="age">
-                        </div>
-
-                        <div class="col-md-6">
-                            <label for="inputCity" class="form-label">شهر</label>
-                            <input type="text" class="form-control" id="inputCity" name="city">
-                        </div>
-
-                        <div class="col-6">
-                            <label for="inputAddress" class="form-label">آدرس</label>
-                            <input type="text" class="form-control" id="inputAddress" name="address" >
-                        </div>
-
-                        <div class="col-6">
-                            <label for="inputState" class="form-label">جنسیت</label>
-                            <select id="inputState" class="form-select" name="sex">
-                                <option value="male" >مرد</option>
-                                <option value="female" >زن</option>
-                            </select>
-                        </div>
+    </header>
 
 
-                        <div class="col-md-6">
-                            <label for="inputState" class="form-label">مدرک تحصیلی</label>
-                            <select id="inputState" class="form-select" name="education">
-                                <option value="diplom">دیپلم</option>
-                                <option value="kardani">کاردانی</option>
-                                <option value="bachelor">لیسانس</option>
-                                <option value="master">ارشد</option>
-                                <option value="phd">دکترا</option>
-                            </select>
-                        </div>
-                        <div class="col-12">
-                            <button type="submit" class="btn btn-primary w-100">ثبت</button>
-                        </div>
-                    </form>
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
 
-                </div>
+                <form class="row g-3" method="POST" action="show.php">
+                    <div class="col-md-6">
+                        <label for="inputEmail4" class="form-label">نام و نام خانوادگی</label>
+                        <input type="text" class="form-control" id="inputEmail4" name="fullname">
+                    </div>
+                    <div class="col-md-6">
+                        <label for="inputPassword4" class="form-label">سن</label>
+                        <input type="number" class="form-control" id="inputPassword4" name="age">
+                    </div>
+
+                    <div class="col-md-6">
+                        <label for="inputCity" class="form-label">استان</label>
+                        <select id="inputState" class="form-select" name="city">
+
+                            <?php
+
+                            $sql = "SELECT name FROM provinces ORDER BY id ASC";
+                            $result = $conn->query($sql);
 
 
+
+                            while ($row = $result->fetch_assoc()) {
+
+                            ?>
+
+                                <option value="<?php echo $row["name"]; ?>"><?php echo $row["name"]; ?></option>
+                            <?php } ?>
+
+                        </select>
+                    </div>
+
+                    <div class="col-6">
+                        <label for="inputAddress" class="form-label">آدرس</label>
+                        <input type="text" class="form-control" id="inputAddress" name="address">
+                    </div>
+
+                    <div class="col-6">
+                        <label for="inputState" class="form-label">جنسیت</label>
+                        <select id="inputState" class="form-select" name="sex">
+                            <option value="male">مرد</option>
+                            <option value="female">زن</option>
+                        </select>
+                    </div>
+
+
+                    <div class="col-md-6">
+                        <label for="inputState" class="form-label">مدرک تحصیلی</label>
+                        <select id="inputState" class="form-select" name="education">
+                            <option value="diplom">دیپلم</option>
+                            <option value="kardani">کاردانی</option>
+                            <option value="bachelor">لیسانس</option>
+                            <option value="master">ارشد</option>
+                            <option value="phd">دکترا</option>
+                        </select>
+                    </div>
+                    <div class="col-12">
+                        <button type="submit" class="btn btn-primary w-100">ثبت</button>
+                    </div>
+                </form>
 
             </div>
+
+
+
         </div>
+    </div>
 
 
 
 
 
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-            crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
 
 
-    </body>
+</body>
 
 </html>
--- show.php
+++ show.php
@@ -83,18 +83,18 @@ VALUES ('$fullname', '$age', '$city','$address','$sex','$education')";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
+
+
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-    <script>
-        $(document).ready(function() {
-            $("#filter").on("keyup", function() {
-                var value = $(this).val().toLowerCase();
-                $("#Table tr").filter(function() {
-                    $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-                });
-            });
-        });
-    </script>
+
+    <script src="asset/js/Filter.js"></script>
+
+
+
+
+
 
     <title>فرم استخدامی</title>
 </head>
@@ -110,54 +110,88 @@ VALUES ('$fullname', '$age', '$city','$address','$sex','$education')";
 
     <div class="container mt-5">
         <div class="row justify-content-center">
+
+
+
+
             <div class="col-md-6">
+
 
                 <h4 style="text-align: center;" class="text-primary">لیست فرم استخدامی </h4>
 
-                <form class="d-flex mb-5">
+                <!-- <form class="d-flex mb-5">
                     <input class="form-control me-2" type="search" aria-label="Search" id="filter">
                     <button class="btn btn-outline-success" type="submit">فیلتر</button>
-                </form>
+                </form> -->
+                <div class="panel panel-primary filterable">
+                    <div class="panel-heading">
+                        <!-- <h3 class="panel-title">
+                            <button class="btn btn-default btn-xs btn-filter bg-primary text-white w-100"><span class="glyphicon glyphicon-filter"></span>فیلتر</button>
+                        </h3> -->
+
+                    </div>
 
 
 
-                <table class="table table-success table-striped table-bordered">
-                    <thead>
-                        <tr>
-                            <th scope="col">ID</th>
-                            <th scope="col">نام و نام خانوادگی </th>
-                            <th scope="col">سن</th>
-                            <th scope="col">شهر</th>
-                            <th scope="col">آدرس</th>
-                            <th scope="col">جنسیت</th>
-                            <th scope="col">مدرک تحصیلی</th>
-                        </tr>
-                    </thead>
-                    <tbody id="Table">
-                        <?php
+                    <table class="table table-success table-striped table-bordered">
+                        <thead>
 
-                        $sql = "SELECT * FROM user ORDER BY id DESC";
-                        $result = $conn->query($sql);
-
-                        while ($row = $result->fetch_assoc()) {
-
-                        ?>
                             <tr>
-                                <th scope="row"><?php echo $row['id']; ?></th>
-                                <td><?php echo $row['fullname']; ?></td>
-                                <td><?php echo $row['age']; ?></td>
-                                <td><?php echo $row['city']; ?></td>
-                                <td><?php echo $row['address']; ?></td>
-                                <td><?php echo $row['sex']; ?></td>
-                                <td><?php echo $row['education']; ?></td>
+                                <th scope="col">فهرست</th>
+                                <th scope="col">نام و نام خانوادگی </th>
+                                <th scope="col">سن</th>
+                                <th scope="col">شهر</th>
+                                <th scope="col">آدرس</th>
+                                <th scope="col">جنسیت</th>
+                                <th scope="col">مدرک تحصیلی</th>
                             </tr>
-                        <?php }
-                        $conn->close();
-                        ?>
-                    </tbody>
-                </table>
+                            <tr class="filters">
+                                <th><input type="text" class="form-control"></th>
+                                <th><input type="text" class="form-control"></th>
+                                <th><input type="text" class="form-control"></th>
+                                <th><input type="text" class="form-control"></th>
+                                <th><input type="text" class="form-control"></th>
+                                <th><input type="text" class="form-control"></th>
+                                <th><input type="text" class="form-control"></th>
+                            </tr>
 
 
+
+
+
+                        </thead>
+                        <tbody id="Table">
+
+
+
+                            <?php
+
+                            $sql = "SELECT * FROM user ORDER BY id DESC";
+                            $result = $conn->query($sql);
+
+                            $i = 1;
+
+                            while ($row = $result->fetch_assoc()) {
+
+                            ?>
+                                <tr>
+
+                                    <td scope="row"><?php echo $i++; ?></td>
+
+                                    <td><?php echo $row['fullname']; ?></td>
+                                    <td><?php echo $row['age']; ?></td>
+                                    <td><?php echo $row['city']; ?></td>
+                                    <td><?php echo $row['address']; ?></td>
+                                    <td><?php echo $row['sex']; ?></td>
+                                    <td><?php echo $row['education']; ?></td>
+                                </tr>
+                            <?php }
+                            $conn->close();
+                            ?>
+                        </tbody>
+                    </table>
+
+                </div>
 
 
 
